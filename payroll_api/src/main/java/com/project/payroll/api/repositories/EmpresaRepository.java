bcb092afc2d4305ddceead1c66b64aaf9e74f746
guikartman <guikartman@gmail.com>
2021-05-23 22:34:57 -0300

    Little refactor.

--- payroll_api/src/main/java/com/project/payroll/api/repositories/EmpresaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.payroll.api.repositories;
-
-import com.project.payroll.api.entities.Empresa;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmpresaRepository extends JpaRepository<Empresa,Long> {
-}
